86c0d28242f3f15641ac87fe03c7f98dddc5d149
Santidlr13 <110190837+Santidlr13@users.noreply.github.com>
2022-09-12 13:52:03 -0400

    First Commit

---
+++ dadjokegen.js
@@ -0,0 +1,32 @@
+let randomNum = Math.floor(Math.random()*10);
+let hitMe = '';
+
+switch (randomNum){
+    case 0:
+        console.log("What do you call a fish wearing a bowtie? Sofishticated.");
+        break;
+    case 1:
+        console.log("Singing in the shower is fun until you get soap in your mouth. Then it's a soap opera.");
+        break;
+    case 2:
+        console.log("What did the ocean say to the beach? Nothing. It just waved.");
+        break;
+    case 3:
+        console.log("What did the janitor say when he jumped out of the closet? Supplies!");
+        break;
+    case 4:
+        console.log("What did one wall say to the other? I'll meet you at the corner.");
+        break;
+    case 5:
+        console.log("My wife is really mad at the fact that I have no sense of direction. So I packed up my stuff and right!");
+        break;
+    case 6:
+        console.log("I don't trust stairs. They're always up to something.");
+        break;
+    case 7:
+        console.log("This graveyard looks overcrowded. People must be dying to get in.");
+        break;
+    case 8:
+        console.log("What kind of shoes do ninjas wear? Sneakers!");
+        break;
+}
